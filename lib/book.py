f9eed972bb644986ac90e543cdc993c08a03b0a2
Shelby Christensen <shelbchrist@yahoo.com>
2025-07-07 22:55:35 -0500

    Fix Book and Coffee class initialization and methods for OOP lab auto-grader

--- lib/book.py
+++ lib/book.py
@@ -3,7 +3,7 @@
 class Book:
     def __init__(self, title, page_count):
         self.title = title
-        self.page_count = page_count  # uses setter for validation
+        self.page_count = page_count
 
     @property
     def page_count(self):
--- lib/coffee.py
+++ lib/coffee.py
@@ -2,7 +2,7 @@
 
 class Coffee:
     def __init__(self, size, price):
-        self.size = size  # uses setter for validation
+        self.size = size
         self.price = price
 
     @property
